e6b9a5446caf72e6440f2fe759e975cd7cb5cd3e
guilhermercoutinho <guilhermecoutinho.rc@gmail.com>
2024-07-31 14:16:02 -0300

    recovery no código

--- src/pages/script.js
+++
@@ -1,9 +0,0 @@
-const li = document.querySelector("li")
-const knowMore = document.querySelector(".know-more")
-
-function displayInformation () {
-    knowMore.style.visibility = "visible";
-    knowMore.style.transitionDelay = "0.5s";
-}
-
-addEventListener("click", displayInformation)
